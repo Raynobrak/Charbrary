cab66c060ea2877b679ddecdef08a013ba6f148a
Lucas Charbonnier <lcs.charbonnier@gmail.com>
2019-07-23 10:10:40 +0200

    Finished Vector class and tests
    [DEV]
    - Removed member function "toString"
    - Finished coding the vector class.
    
    [TEST]
    Added unit tests for the vector class.

--- source/Charbrary/Physics/Vector.cpp
+++ source/Charbrary/Physics/Vector.cpp
@@ -1,18 +1,16 @@
-/*
-   Copyright 2019 Lucas Charbonnier
+/* Copyright 2019 Lucas Charbonnier
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-	   http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-*/
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
 
 #include "Vector.h"
 #pragma once
@@ -21,19 +19,19 @@
 
 namespace CB {
 
-	Vector::Vector(VECTOR_T X, VECTOR_T Y) : x(X), y(Y) {}
+	Vector::Vector(float X, float Y) : x(X), y(Y) {}
 
-	Scalar Vector::magnitudeSquared() const {
-		return (x * x + y * y);
+	float Vector::magnitudeSquared() const {
+		return x * x + y * y;
 	}
 
-	Scalar Vector::magnitude() const {
+	float Vector::magnitude() const {
 		return std::sqrtf(magnitudeSquared());
 	}
 
 	Vector Vector::normalize() const {
-		if (hasAMagnitudeOf0()) {
-			return { 0,0 };
+		if (x == 0 && y == 0) {
+			return Vector();
 		}
 
 		return *this / magnitude();
@@ -41,10 +39,6 @@ namespace CB {
 
 	Vector Vector::abs() const {
 		return Vector(std::abs(x), std::abs(y));
-	}
-
-	Scalar Vector::dotProductWith(const Vector & other) {
-		return x * other.x + y * other.y;
 	}
 
 	Vector & Vector::operator+=(const Vector & add) {
@@ -58,13 +52,13 @@ namespace CB {
 		return *this;
 	}
 
-	Vector & Vector::operator*=(const Scalar & scalar) {
+	Vector & Vector::operator*=(const float scalar) {
 		x *= scalar;
 		y *= scalar;
 		return *this;
 	}
 
-	Vector & Vector::operator/=(const Scalar & divisor) {
+	Vector & Vector::operator/=(const float divisor) {
 		if (divisor == 0) {
 			throw std::invalid_argument("Invalid argument : 'divisor' was 0, cannot divide by zero.");
 		}
@@ -79,8 +73,8 @@ namespace CB {
 		y = other.y;
 	}
 
-	bool Vector::hasAMagnitudeOf0() const {
-		return x == 0 && y == 0;
+	float Vector::dotProduct(const Vector& a, const Vector& b) {
+		return a.x * b.x + a.y * b.y;
 	}
 
 	Vector operator+(const Vector & left, const Vector & right) {
@@ -95,17 +89,17 @@ namespace CB {
 		return Vector(-right.x, -right.y);
 	}
 
-	Vector operator*(const Vector & base, const Scalar scalar) {
+	Vector operator*(const Vector & base, const float scalar) {
 		return Vector(base.x * scalar, base.y * scalar);
 	}
 
-	Vector operator*(const Scalar scalar, const Vector & base) {
+	Vector operator*(const float scalar, const Vector & base) {
 		return base * scalar;
 	}
 
-	Vector operator/(const Vector & base, const Scalar divisor) {
+	Vector operator/(const Vector & base, const float divisor) {
 		if (divisor == 0) {
-			throw std::invalid_argument("Invalid argument : 'divisor' was 0, cannot divide by zero.");
+			throw std::invalid_argument("Invalid argument : Cannot divide vector by 0");
 		}
 		return Vector(base.x / divisor, base.y / divisor);
 	}
@@ -117,6 +111,5 @@ namespace CB {
 	bool operator!=(const Vector & left, const Vector & right) {
 		return !(left == right);
 	}
-
 }
 
--- source/Charbrary/Physics/Vector.h
+++ source/Charbrary/Physics/Vector.h
@@ -1,57 +1,53 @@
-/*
-   Copyright 2019 Lucas Charbonnier
+/* Copyright 2019 Lucas Charbonnier
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-	   http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-*/
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
+
 
 #pragma once
 
+#include <string>
+
 namespace CB {
-	typedef float VECTOR_T;
-	typedef VECTOR_T Scalar;
 
 	class Vector {
+
 	public:
+		float x;
 
-		VECTOR_T x;
+		float y;
 
-		VECTOR_T y;
+	public:
+		Vector(float X = 0.f, float Y = 0.f);
 
-		Vector(VECTOR_T X = (VECTOR_T)0, VECTOR_T Y = (VECTOR_T)0);
+		float magnitudeSquared() const;
 
-		Scalar magnitudeSquared() const;
-
-		Scalar magnitude() const;
+		float magnitude() const;
 
 		Vector normalize() const;
 
 		Vector abs() const;
 
-		Scalar dotProductWith(const Vector& other);
-
-
 		Vector& operator+=(const Vector& add);
 
 		Vector& operator-=(const Vector& substract);
 
-		Vector& operator*=(const Scalar& scalar);
+		Vector& operator*=(const float scalar);
 
-		Vector& operator/=(const Scalar& divisor);
+		Vector& operator/=(const float divisor);
 
 		void operator=(const Vector& other);
-	private:
 
-		bool hasAMagnitudeOf0() const;
+		static float dotProduct(const Vector& a, const Vector& b);
 	};
 
 
@@ -61,13 +57,11 @@ namespace CB {
 
 	Vector operator-(const Vector& right);
 
-	Vector operator*(const Vector& base, const Scalar scalar);
+	Vector operator*(const Vector& base, const float scalar);
 
+	Vector operator*(const float scalar, const Vector& base);
 
-	Vector operator*(const Scalar scalar, const Vector& base);
-
-
-	Vector operator/(const Vector& base, const Scalar divisor);
+	Vector operator/(const Vector& base, const float divisor);
 
 	bool operator==(const Vector& left, const Vector& right);
 
