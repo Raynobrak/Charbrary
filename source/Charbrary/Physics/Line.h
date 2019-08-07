87bda416beece1901376f184c52f33fe05a95c2d
Lucas Charbonnier <charbonnlu@etml.educanet2.ch>
2019-08-07 17:46:48 +0200

    [DEV] Started the Line class.

---
+++ source/Charbrary/Physics/Line.h
@@ -0,0 +1,43 @@
+/* Copyright 2019 Lucas Charbonnier
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
+
+#pragma once
+
+#include "Vector.h"
+
+namespace CB {
+
+	class Line {
+
+	public:
+
+		Vector start;
+		Vector end;
+		bool isRight;
+
+	public:
+
+		Line(const Vector& start, const Vector& end, bool isRight);
+
+		Line(const Vector& start, const Vector& size);
+
+		float length() const;
+
+		float dir() const;
+
+		float distance(const Vector& point) const;
+
+		bool intersects(const Line& other) const;
+	};
+}
