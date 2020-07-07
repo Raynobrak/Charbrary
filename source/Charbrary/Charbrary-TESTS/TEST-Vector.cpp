6e57402fd68ffbdd069e211ce73256afccbfb88f
Charbonnier Lucas <lcharbonnier@vaudoise.ch>
2020-07-07 15:15:43 +0200

    [DEV[ Changed the vector maths functions name in the test functions.

--- source/Charbrary/Charbrary-TESTS/TEST-Vector.cpp
+++ source/Charbrary/Charbrary-TESTS/TEST-Vector.cpp
@@ -28,21 +28,21 @@ TEST(Vector, ctor_default) {
 }
 
 TEST(Vector, polar_coordinates_0degrees) {
-	CB::vec_t v = CB::fromPolarCoordinates(0.f, 10.f);
+	CB::vec_t v = CB::vec_from_polar_coordinates(0.f, 10.f);
 
 	EXPECT_FLOAT_EQ(v.x, 10.f);
 	EXPECT_FLOAT_EQ(v.y, 0.f);
 }
 
 TEST(Vector, polar_coordinates_90degrees) {
-	CB::vec_t v = CB::fromPolarCoordinates(90.f, 2.f);
+	CB::vec_t v = CB::vec_from_polar_coordinates(90.f, 2.f);
 
 	EXPECT_NEAR(v.x, 0.f, FLOAT_EPSILON);
 	EXPECT_NEAR(v.y, 2.f, FLOAT_EPSILON);
 }
 
 TEST(Vector, polar_coordinates_negative_angle) {
-	CB::vec_t v = CB::fromPolarCoordinates(-120.f, 1.f);
+	CB::vec_t v = CB::vec_from_polar_coordinates(-120.f, 1.f);
 
 	EXPECT_NEAR(v.x, -0.5f, FLOAT_EPSILON);
 	EXPECT_NEAR(v.y, -sqrtf(3.f)/2.f, FLOAT_EPSILON);
@@ -51,45 +51,45 @@ TEST(Vector, polar_coordinates_negative_angle) {
 TEST(Vector, magnitudeSquared) {
 	CB::vec_t v(3.f, 4.f);
 
-	EXPECT_FLOAT_EQ(CB::magnitudeSquared(v), 25.f);
+	EXPECT_FLOAT_EQ(CB::vec_magnitude_squared(v), 25.f);
 }
 
 TEST(Vector, magnitude) {
 	CB::vec_t v(-4.f, 3.f);
 
-	EXPECT_FLOAT_EQ(CB::magnitude(v), 5.f);
+	EXPECT_FLOAT_EQ(CB::vec_magnitude(v), 5.f);
 }
 
 TEST(Vector, normalize) {
 	CB::vec_t v(3.f, 4.f);
 	CB::vec_t expected(0.6f, 0.8f);
 
-	EXPECT_EQ(CB::normalize(v), expected);
+	EXPECT_EQ(CB::vec_normalize(v), expected);
 }
 
 TEST(Vector, normalize_div0) {
 	CB::vec_t v(0.f, 0.f);
 
-	EXPECT_EQ(CB::normalize(v), v);
+	EXPECT_EQ(CB::vec_normalize(v), v);
 }
 
 TEST(Vector, abs) {
 	CB::vec_t v(-456.f, 201.f);
 	CB::vec_t expected(456.f, 201.f);
 
-	EXPECT_EQ(CB::abs(v), expected);
+	EXPECT_EQ(CB::vec_abs(v), expected);
 }
 
 TEST(Vector, rotate_0degrees) {
 	CB::vec_t v(12.f, 31.f);
-	CB::vec_t after = CB::rotate(v, 0.f);
+	CB::vec_t after = CB::vec_rotate(v, 0.f);
 
 	EXPECT_EQ(v, after);
 }
 
 TEST(Vector, rotate_45degrees) {
 	CB::vec_t v(0.1f, 0.1f);
-	CB::vec_t after = CB::rotate(v, 45.f);
+	CB::vec_t after = CB::vec_rotate(v, 45.f);
 
 	EXPECT_NEAR(after.x, 0.f, FLOAT_EPSILON);
 	EXPECT_NEAR(after.y, sqrtf(2.f) / 10.f, FLOAT_EPSILON);
@@ -97,7 +97,7 @@ TEST(Vector, rotate_45degrees) {
 
 TEST(Vector, rotate_minus180degrees) {
 	CB::vec_t v(0.3f, 0.2f);
-	CB::vec_t after = CB::rotate(v, -180.f);
+	CB::vec_t after = CB::vec_rotate(v, -180.f);
 
 	EXPECT_NEAR(after.x, -v.x, FLOAT_EPSILON);
 	EXPECT_NEAR(after.y, -v.y, FLOAT_EPSILON);
@@ -105,7 +105,7 @@ TEST(Vector, rotate_minus180degrees) {
 
 TEST(Vector, rotate_360degrees) {
 	CB::vec_t v(0.4f, 0.1f);
-	CB::vec_t after = CB::rotate(v, 360.f);
+	CB::vec_t after = CB::vec_rotate(v, 360.f);
 
 	EXPECT_NEAR(v.x, after.x, FLOAT_EPSILON);
 	EXPECT_NEAR(v.y, after.y, FLOAT_EPSILON);
@@ -170,7 +170,7 @@ TEST(Vector, dotProduct) {
 
 	float expected = -5.f;
 
-	EXPECT_FLOAT_EQ(CB::dotProduct(first, second), expected);
+	EXPECT_FLOAT_EQ(CB::vec_dot_product(first, second), expected);
 }
 
 TEST(Vector, add_operator) {
