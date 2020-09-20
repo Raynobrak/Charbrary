a56f727495c9073e3a02ac6432fe3b9909a860b8
Lucas Charbonnier <lcs.charbonnier@gmail.com>
2020-09-20 20:48:44 +0200

    Added vector constants for directions.

--- source/Charbrary/Physics/Constants.h
+++ source/Charbrary/Physics/Constants.h
@@ -1,5 +1,14 @@
 #pragma once
 
+#include "vector_type_definition.h"
+
 namespace CB {
 	constexpr float FLT_PI = 3.14159265359f;
+
+	static const CB::vec_t LEFT_VEC = { -1.f, 0.f };
+	static const CB::vec_t RIGHT_VEC = { 1.f, 0.f };
+	static const CB::vec_t UP_VEC = { 0.f, 1.f };
+	static const CB::vec_t DOWN_VEC = { 0.f, -1.f };
+
+	static const CB::vec_t NULL_VEC = { 0.f,0.f };
 }
