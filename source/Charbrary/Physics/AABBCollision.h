dfc2b0647f575f2187c9d259a9b375d47771f7db
Lucas Charbonnier <lcs.charbonnier@gmail.com>
2020-09-20 20:47:12 +0200

    Added the AABBCollision struct. Its purpose is to contain the details of a collision between 2 AABBs.

---
+++ source/Charbrary/Physics/AABBCollision.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "vector_type_definition.h"
+#include "Constants.h"
+
+namespace CB {
+
+	/*
+	 * \brief Contains information about the collision of 2 AABBs.
+	 */
+	struct AABBCollision {
+		vec_t normal; /**< The collision normal, a vector representing the direction of the collision. */
+		vec_t delta; /**< The overlap of the colliding AABB on the other AABB. */
+	};
+
+	
+}
