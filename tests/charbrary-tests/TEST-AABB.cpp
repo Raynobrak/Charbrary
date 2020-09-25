52b6232db190506ebf9e76ca03ac29299e8d1142
Lucas Charbonnier <lcs.charbonnier@gmail.com>
2020-09-25 15:33:58 +0200

    Improved test names for the AABB class

--- tests/charbrary-tests/TEST-AABB.cpp
+++ tests/charbrary-tests/TEST-AABB.cpp
@@ -4,7 +4,7 @@
 
 #include <array>
 
-TEST_CASE("AABB, ctor_vectors") {
+TEST_CASE("construct aabb from 2 vectors", "[AABB]") {
 	CB::AABB aabb(CB::vec_t(3.f, 5.f), CB::vec_t(9.f, -8.f));
 
 	REQUIRE(aabb.pos.x == 3.f);
@@ -13,7 +13,7 @@ TEST_CASE("AABB, ctor_vectors") {
 	REQUIRE(aabb.size.y == -8.f);
 }
 
-TEST_CASE("AABB, ctor_floats") {
+TEST_CASE("construct aabb from 4 floats", "[AABB]") {
 	CB::AABB aabb(3.f, 5.f, 9.f, -8.f);
 
 	REQUIRE(aabb.pos.x == 3.f);
@@ -22,7 +22,7 @@ TEST_CASE("AABB, ctor_floats") {
 	REQUIRE(aabb.size.y == -8.f);
 }
 
-TEST_CASE("AABB, ctor_default") {
+TEST_CASE("default construct aabb", "[AABB]") {
 	CB::AABB aabb;
 
 	REQUIRE(aabb.pos.x == 0.f);
@@ -31,7 +31,7 @@ TEST_CASE("AABB, ctor_default") {
 	REQUIRE(aabb.size.y == 0.f);
 }
 
-TEST_CASE("AABB, move") {
+TEST_CASE("move aabb with a translation vector", "[AABB]") {
 	CB::AABB aabb(1.f, 8.f, 5.f, 5.f);
 
 	aabb.move({ -7.f,2.f });
@@ -41,48 +41,48 @@ TEST_CASE("AABB, move") {
 	REQUIRE(aabb == expected);
 }
 
-TEST_CASE("AABB, center") {
+TEST_CASE("compute aabb center position", "[AABB]") {
 	CB::AABB aabb(3.f, 5.f, 10.f, 20.f);
 	CB::vec_t expected(8.f, 15.f);
 
 	REQUIRE(aabb.center() == expected);
 }
 
-TEST_CASE("AABB, corner_tl") {
+TEST_CASE("compute top left corner position of aabb", "[AABB]") {
 	CB::AABB aabb(3.f, 5.f, 10.f, 20.f);
 	CB::vec_t expected(3.f, 5.f);
 
 	REQUIRE(aabb.corner(CB::Corner::TopLeft) == expected);
 }
 
-TEST_CASE("AABB, corner_tr") {
+TEST_CASE("compute top right corner position of aabb", "[AABB]") {
 	CB::AABB aabb(3.f, 5.f, 10.f, 20.f);
 	CB::vec_t expected(13.f, 5.f);
 
 	REQUIRE(aabb.corner(CB::Corner::TopRight) == expected);
 }
 
-TEST_CASE("AABB, corner_bl") {
+TEST_CASE("compute bottom left corner position of aabb", "[AABB]") {
 	CB::AABB aabb(3.f, 5.f, 10.f, 20.f);
 	CB::vec_t expected(3.f, 25.f);
 
 	REQUIRE(aabb.corner(CB::Corner::BottomLeft) == expected);
 }
 
-TEST_CASE("AABB, corner_br") {
+TEST_CASE("compute bottom right corner position of aabb", "[AABB]") {
 	CB::AABB aabb(3.f, 5.f, 10.f, 20.f);
 	CB::vec_t expected(13.f, 25.f);
 
 	REQUIRE(aabb.corner(CB::Corner::BottomRight) == expected);
 }
 
-TEST_CASE("AABB, corner_invalid") {
+TEST_CASE("try to compute invalid corner position of aabb", "[AABB]") {
 	CB::AABB aabb(3.f, 5.f, 10.f, 20.f);
 
 	REQUIRE_THROWS_AS(aabb.corner(static_cast<CB::Corner>(434923)), std::invalid_argument);
 }
  
-TEST_CASE("AABB, corners") {
+TEST_CASE("compute the position of all 4 corners of aabb", "[AABB]") {
 	CB::AABB aabb(3.f, 5.f, 10.f, 20.f);
 
 	auto corners = aabb.corners();
@@ -93,42 +93,42 @@ TEST_CASE("AABB, corners") {
 	REQUIRE(corners[3] == aabb.corner(CB::Corner::BottomRight));
 }
 
-TEST_CASE("AABB, detectCollision_no_collision") {
+TEST_CASE("getting collision information when there is no collision between 2 aabbs", "[AABB]") {
 	CB::AABB current(0.f, 0.f, 10.f, 10.f);
 	CB::AABB other(50.f, 50.f, 30.f, 30.f);
 
 	REQUIRE(current.detectCollision(other).normal == CB::NULL_VEC);
 }
 
-TEST_CASE("AABB, detectCollision_left") {
+TEST_CASE("aabb collides with the left side of the calling aabb", "[AABB]") {
 	CB::AABB current(0.f, 0.f, 10.f, 10.f);
 	CB::AABB other(-1.f, 1.f, 3.f, 3.f);
 
 	REQUIRE(current.detectCollision(other).normal == CB::LEFT_VEC);
 }
 
-TEST_CASE("AABB, detectCollision_right") {
+TEST_CASE("aabb collides with the right side of the calling aabb", "[AABB]") {
 	CB::AABB current(0.f, 0.f, 10.f, 10.f);
 	CB::AABB other(8.f, 5.f, 40.f, 40.f);
 
 	REQUIRE(current.detectCollision(other).normal == CB::RIGHT_VEC);
 }
 
-TEST_CASE("AABB, detectCollision_up") {
+TEST_CASE("aabb collides with the top side of the calling aabb", "[AABB]") {
 	CB::AABB current(10.f, 10.f, 4.f, 4.f);
 	CB::AABB other(9.f, 9.f, 4.f, 2.f);
 
 	REQUIRE(current.detectCollision(other).normal == CB::UP_VEC);
 }
 
-TEST_CASE("AABB, detectCollision_down") {
+TEST_CASE("aabb collides with the bottom side of the calling aabb", "[AABB]") {
 	CB::AABB current(0.f, 0.f, 10.f, 10.f);
 	CB::AABB other(5.f, 9.f, 10.f, 10.f);
 
 	REQUIRE(current.detectCollision(other).normal == CB::DOWN_VEC);
 }
 
-TEST_CASE("AABB, scaleRelativeToCenter") {
+TEST_CASE("scale an aabb relative to its center by a factor smaller than 1", "[AABB]") {
 	CB::AABB aabb(3.f, 5.f, 10.f, 20.f);
 	CB::vec_t centerPosBefore = aabb.center();
 	CB::vec_t expectedSize(6.f, 12.f);
@@ -139,41 +139,52 @@ TEST_CASE("AABB, scaleRelativeToCenter") {
 	REQUIRE(aabb.size == expectedSize);
 }
 
-TEST_CASE("AABB, perimeter") {
+TEST_CASE("scale an aabb relative to its center by a factor greater than 1", "[AABB]") {
+	CB::AABB aabb(3.f, 5.f, 10.f, 20.f);
+	CB::vec_t centerPosBefore = aabb.center();
+	CB::vec_t expectedSize(20.f, 40.f);
+
+	aabb.scaleRelativeToCenter(2.f);
+
+	REQUIRE(aabb.center() == centerPosBefore);
+	REQUIRE(aabb.size == expectedSize);
+}
+
+TEST_CASE("compute the perimeter of an aabb", "[AABB]") {
 	CB::AABB aabb(0.f, 0.f, 1.f, 3.f);
 	float expected = 8.f;
 
 	REQUIRE(aabb.perimeter() == expected);
 }
 
-TEST_CASE("AABB, area") {
+TEST_CASE("compute the area of an aabb", "[AABB]") {
 	CB::AABB aabb(0.f, 0.f, 2.f, 3.f);
 	float expected = 6.f;
 
 	REQUIRE(aabb.area() == expected);
 }
 
-TEST_CASE("AABB, diagonalLength") {
+TEST_CASE("compute the length of an aabb's diagonals", "[AABB]") {
 	CB::AABB aabb(0.f, 0.f, 3.f, 4.f);
 	float expected = 5.f;
 
 	REQUIRE(aabb.diagonalLength() == expected);
 }
 
-TEST_CASE("AABB, contains_true") {
+TEST_CASE("aabb contains a point", "[AABB]") {
 	CB::AABB aabb(6.f, 7.f, 10.f, 20.f);
 	CB::vec_t point(6.f, 26.f);
 
 	REQUIRE(aabb.contains(point));
 }
 
-TEST_CASE("AABB, contains_false") {
+TEST_CASE("aabb doesn't contain a point", "[AABB]") {
 	CB::AABB aabb(6.f, 7.f, 10.f, 20.f);
 	CB::vec_t point(5.f, 14.f);
 
 	REQUIRE_FALSE(aabb.contains(point));}
 
-TEST_CASE("AABB, intersects_false") {
+TEST_CASE("aabbs intersect", "[AABB]") {
 	CB::AABB first(6.f, 7.f, 10.f, 20.f);
 	CB::AABB second(18.f, 10.f, 30.f, 40.f);
 
@@ -181,7 +192,7 @@ TEST_CASE("AABB, intersects_false") {
 	REQUIRE_FALSE(second.intersects(first));
 }
 
-TEST_CASE("AABB, intersects_true_inside") {
+TEST_CASE("aabbs don't intersect", "[AABB]") {
 	CB::AABB first(6.f, 4.f, 10.f, 5.f);
 	CB::AABB second(7.f, 5.f, 5.f, 2.f);
 
@@ -189,13 +200,13 @@ TEST_CASE("AABB, intersects_true_inside") {
 	REQUIRE(second.intersects(first));
 }
 
-TEST_CASE("AABB, intersects_true_itself") {
+TEST_CASE("aabb intersects itself", "[AABB]") {
 	CB::AABB aabb(6.f, 4.f, 10.f, 5.f);
 
 	REQUIRE(aabb.intersects(aabb));
 }
 
-TEST_CASE("AABB, intersects_true_cross") {
+TEST_CASE("crossing aabbs intersect", "[AABB]") {
 	CB::AABB first(6.f, 4.f, 10.f, 30.f);
 	CB::AABB second(1.f, 10.f, 30.f, 10.f);
 
@@ -203,14 +214,14 @@ TEST_CASE("AABB, intersects_true_cross") {
 	REQUIRE(second.intersects(first));
 }
 
-TEST_CASE("AABB, strictlyContains_false") {
+TEST_CASE("aabb does not strictly contain another aabb", "[AABB]") {
 	CB::AABB first(6.f, 4.f, 10.f, 30.f);
 	CB::AABB second(5.f, 6.f, 4.f, 3.f);
 
 	REQUIRE_FALSE(first.strictlyContains(second));
 }
 
-TEST_CASE("AABB, strictlyContains_true") {
+TEST_CASE("aabb strictly contains another aabb but the opposite is not true", "[AABB]") {
 	CB::AABB container(2.f, 3.f, 50.f, 20.f);
 	CB::AABB contained(10.f, 10.f, 5.f, 5.f);
 
@@ -218,13 +229,13 @@ TEST_CASE("AABB, strictlyContains_true") {
 	REQUIRE_FALSE(contained.strictlyContains(container));
 }
 
-TEST_CASE("AABB, strictlyContains_true_itself") {
+TEST_CASE("aabb strictly contains itself", "[AABB]") {
 	CB::AABB aabb(6.f, 4.f, 10.f, 30.f);
 	
 	REQUIRE(aabb.strictlyContains(aabb));
 }
 
-TEST_CASE("AABB, assign_operator") {
+TEST_CASE("aabb assignment operator", "[AABB]") {
 	CB::AABB aabb(6.f, 4.f, 10.f, 30.f);
 	CB::AABB expected(19.f, 25.f, 45.f, 898.f);
 	aabb = expected;
@@ -232,14 +243,14 @@ TEST_CASE("AABB, assign_operator") {
 	REQUIRE(aabb == expected);
 }
 
-TEST_CASE("AABB, equality_operator") {
+TEST_CASE("aabb equality operator", "[AABB]") {
 	CB::AABB first(1.f, 2.f, 3.f, 4.f);
 	CB::AABB second(1.f, 2.f, 3.f, 4.f);
 
 	REQUIRE(first == second);
 }
 
-TEST_CASE("AABB, inequality_operator") {
+TEST_CASE("aabb inequality operator", "[AABB]") {
 	CB::AABB first(1.f, 2.f, 3.f, 4.f);
 	CB::AABB second(4.f, 3.f, 2.f, 1.f);
 
