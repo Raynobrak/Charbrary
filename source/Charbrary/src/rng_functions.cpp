96e51d19f1fc7ae7c5bd5cc4be014e835fbc3489
Lucas Charbonnier <lcs.charbonnier@gmail.com>
2021-04-21 08:40:38 +0200

    put rng utils in a dedicated namespace (ch::rand::)

--- source/Charbrary/src/rng_functions.cpp
+++ source/Charbrary/src/rng_functions.cpp
@@ -4,61 +4,71 @@
 #include <chrono>
 
 namespace ch {
+	namespace rand {
 
-	/**
-	 * \brief Returns a reference to a static instance of the random engine.
-	 */
-	std::default_random_engine& get_random_engine() {
-		static std::default_random_engine rng{ static_cast<unsigned int>(std::chrono::system_clock::now().time_since_epoch().count()) };
-		return rng;
-	}
+		/**
+		 * \brief Returns a reference to a static instance of the random engine.
+		 */
+		std::default_random_engine& get_random_engine() {
+			static std::default_random_engine rng{ static_cast<unsigned int>(std::chrono::system_clock::now().time_since_epoch().count()) };
+			return rng;
+		}
 
-	int rnd_int(int lowerInc, int upperInc) {
-		std::uniform_int_distribution<int> distribution(lowerInc, upperInc);
-		return distribution(get_random_engine());
-	}
+		int rand_int(int lowerInc, int upperInc) {
+			std::uniform_int_distribution<int> distribution(lowerInc, upperInc);
+			return distribution(get_random_engine());
+		}
 
-	float rnd_float(float min, float max) {
-		std::uniform_real_distribution<float> distribution(min, max);
-		return distribution(get_random_engine());
-	}
+		float rand_float(float min, float max) {
+			std::uniform_real_distribution<float> distribution(min, max);
+			return distribution(get_random_engine());
+		}
 
-	float rnd_normal_float() {
-		return rnd_float(-1.f, 1.f);
-	}
+		bool rand_bit() {
+			return rand_int(0, 1) == 0;
+		}
 
-	float rnd_angle_deg() {
-		return rnd_float(0.f, 360.f);
-	}
+		bool rand_bit(float probability) {
+			return rand_float(0.f, 1.f) <= probability;
+		}
 
-	float rnd_angle_rad() {
-		static constexpr float PI_2 = 2.f * 3.1415926f;
-		return rnd_float(0.f, PI_2);
-	}
+		float rnd_normal_float() {
+			return rand_float(-1.f, 1.f);
+		}
 
-	vec_t rnd_sfvector(float minX, float maxX, float minY, float maxY) {
-		return vec_t(rnd_float(minX, maxX), rnd_float(minY, maxY));
-	}
+		float rnd_angle_deg() {
+			return rand_float(0.f, 360.f);
+		}
 
-	vec_t rnd_unit_sfvector() {
-		float randomRadianAngle = rnd_angle_rad();
-		return vec_t(std::cosf(randomRadianAngle), std::sinf(randomRadianAngle));
-	}
+		float rnd_angle_rad() {
+			static constexpr float PI_2 = 2.f * 3.1415926f;
+			return rand_float(0.f, PI_2);
+		}
 
-	vec_t rnd_point_on_rect(vec_t topLeftCorner, vec_t size) {
-		auto bottomRightCorner = topLeftCorner + size;
-		return rnd_sfvector(topLeftCorner.x, topLeftCorner.y, bottomRightCorner.x, bottomRightCorner.y);
-	}
+		vec_t rand_vector(float minX, float maxX, float minY, float maxY) {
+			return vec_t(rand_float(minX, maxX), rand_float(minY, maxY));
+		}
 
-	vec_t rnd_point_on_rect(vec_t center, float width, float height) {
-		return center + vec_t(rnd_normal_float() * (width / 2.f), rnd_normal_float() * (height / 2.f));
-	}
+		vec_t rand_unit_vector() {
+			float randomRadianAngle = rnd_angle_rad();
+			return vec_t(std::cosf(randomRadianAngle), std::sinf(randomRadianAngle));
+		}
 
-	vec_t rnd_point_on_circle(float circleRadius, vec_t circleCenter) {
-		return circleCenter + rnd_unit_sfvector() * rnd_float(0.f, circleRadius);
-	}
+		vec_t rand_point_on_rect(vec_t topLeftCorner, vec_t size) {
+			auto bottomRightCorner = topLeftCorner + size;
+			return rand_vector(topLeftCorner.x, topLeftCorner.y, bottomRightCorner.x, bottomRightCorner.y);
+		}
 
-	vec_t rnd_point_on_torus(float innerRadius, float outerRadius, vec_t torusCenter) {
-		return torusCenter + rnd_unit_sfvector() * rnd_float(innerRadius, outerRadius);
+		vec_t rand_point_on_rect(vec_t center, float width, float height) {
+			return center + vec_t(rnd_normal_float() * (width / 2.f), rnd_normal_float() * (height / 2.f));
+		}
+
+		vec_t rand_point_on_circle(float circleRadius, vec_t circleCenter) {
+			return circleCenter + rand_unit_vector() * rand_float(0.f, circleRadius);
+		}
+
+		vec_t rand_point_on_torus(float innerRadius, float outerRadius, vec_t torusCenter) {
+			return torusCenter + rand_unit_vector() * rand_float(innerRadius, outerRadius);
+		}
 	}
 }
--- source/Charbrary/src/rng_functions.h
+++ source/Charbrary/src/rng_functions.h
@@ -2,60 +2,74 @@
 
 #include "vector_type_definition.h"
 
-namespace ch {
+#include <vector>
 
-	/**
-	 * \brief Generates a random integer within the given boundaries. The lower and upper limits are inclusive, meaning they are included in the range of possible values.
-	 */
-	int rnd_int(int lowerInc, int upperInc);
+namespace ch { 
+	namespace rand {
 
-	/**
-	 * \brief Generates a random float within the given boundaries. The lower and upper limits are inclusive, meaning they are included in the range of possible values.
-	 */
-	float rnd_float(float min, float max);
+		/**
+		 * \brief Generates a random integer within the given boundaries. The lower and upper limits are inclusive, meaning they are included in the range of possible values.
+		 */
+		int rand_int(int lowerInc, int upperInc);
 
-	/**
-	 * \brief Generates a random normalized float (between -1 and 1).
-	 */
-	float rnd_normal_float();
+		/**
+		 * \brief Generates a random float within the given boundaries. The lower and upper limits are inclusive, meaning they are included in the range of possible values.
+		 */
+		float rand_float(float min, float max);
 
-	/**
-	 * \brief Generates a random angle in degrees (between 0 and 360).
-	 */
-	float rnd_angle_deg();
+		/**
+		* \brief Returns a random boolean.
+		*/
+		bool rand_bit();
+		 
+		/**
+		* \brief Returns a boolean with the given probability of being true.
+		*/
+		bool rand_bit(float probability);
 
-	/**
-	 * \brief Generates a random angle in radians (between 0 and 2*pi).
-	 */
-	float rnd_angle_rad();
+		/**
+		* \brief Generates a random normalized float (between -1 and 1).
+		*/
+		float rnd_normal_float();
 
-	/**
-	 * \brief Generates a vector with random XY values within the given boundaries.
-	 */
-	vec_t rnd_sfvector(float minX, float maxX, float minY, float maxY);
+		/**
+		 * \brief Generates a random angle in degrees (between 0 and 360).
+		 */
+		float rnd_angle_deg();
 
-	/**
-	 * \brief Generates a random unit vector.
-	 */
-	vec_t rnd_unit_sfvector();
+		/**
+		 * \brief Generates a random angle in radians (between 0 and 2*pi).
+		 */
+		float rnd_angle_rad();
 
-	/**
-	 * \brief Generates a random point located on the given rectangle.
-	 */
-	vec_t rnd_point_on_rect(vec_t topLeftCorner, vec_t size);
+		/**
+		 * \brief Generates a vector with random XY values within the given boundaries.
+		 */
+		vec_t rand_vector(float minX, float maxX, float minY, float maxY);
 
-	/**
-	 * \brief Generates a random point located on the given rectangle.
-	 */
-	vec_t rnd_point_on_rect(vec_t center, float width, float height);
+		/**
+		 * \brief Generates a random unit vector.
+		 */
+		vec_t rand_unit_vector();
 
-	/**
-	 * \brief Generates a random point located on the given circle.
-	 */
-	vec_t rnd_point_on_circle(float circleRadius, vec_t circleCenter = { 0.f,0.f });
+		/**
+		 * \brief Generates a random point located on the given rectangle.
+		 */
+		vec_t rand_point_on_rect(vec_t topLeftCorner, vec_t size);
 
-	/**
-	 * \brief Generates a random point located on the given taurus (taurus = donut).
-	 */
-	vec_t rnd_point_on_torus(float innerRadius, float outerRadius, vec_t torusCenter = { 0.f,0.f });
+		/**
+		 * \brief Generates a random point located on the given rectangle.
+		 */
+		vec_t rand_point_on_rect(vec_t center, float width, float height);
+
+		/**
+		 * \brief Generates a random point located on the given circle.
+		 */
+		vec_t rand_point_on_circle(float circleRadius, vec_t circleCenter = { 0.f,0.f });
+
+		/**
+		 * \brief Generates a random point located on the given taurus (taurus = donut).
+		 */
+		vec_t rand_point_on_torus(float innerRadius, float outerRadius, vec_t torusCenter = { 0.f,0.f });
+	}
 }
