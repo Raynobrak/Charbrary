f6ba776df435acebaf1700e2b3aa6e82ff7bc388
Lucas Charbonnier <lcs.charbonnier@gmail.com>
2021-04-21 11:34:47 +0200

    added utility functions to find horizontal or vertical opposite of a corner

--- source/Charbrary/src/Corner.cpp
+++ source/Charbrary/src/Corner.cpp
@@ -11,7 +11,28 @@ namespace ch {
 			Corner::TopRight,
 			Corner::TopLeft
 		};
-
 		return DIAGONALLY_OPPOSED_CORNERS[static_cast<size_t>(corner)];
+	}
+
+	constexpr Corner vertically_opposed_corner(Corner corner) {
+		constexpr std::array<Corner, static_cast<size_t>(Corner::MAX_VALUE)> VERTICALLY_OPPOSED_CORNERS =
+		{
+			Corner::BottomLeft,
+			Corner::BottomRight,
+			Corner::TopLeft,
+			Corner::TopRight
+		};
+		return VERTICALLY_OPPOSED_CORNERS[static_cast<size_t>(corner)];
+	}
+
+	constexpr Corner horizontally_opposed_corner(Corner corner) {
+		constexpr std::array<Corner, static_cast<size_t>(Corner::MAX_VALUE)> HORIZONTALLY_OPPOSED_CORNERS =
+		{
+			vertically_opposed_corner(diagonally_opposed_corner(Corner::TopLeft)),
+			vertically_opposed_corner(diagonally_opposed_corner(Corner::TopRight)),
+			vertically_opposed_corner(diagonally_opposed_corner(Corner::BottomLeft)),
+			vertically_opposed_corner(diagonally_opposed_corner(Corner::BottomRight))
+		};
+		return HORIZONTALLY_OPPOSED_CORNERS[static_cast<size_t>(corner)];
 	}
 }
--- source/Charbrary/src/Corner.h
+++ source/Charbrary/src/Corner.h
@@ -13,7 +13,7 @@ namespace ch {
 	};
 
 	/**
-	 * \brief Utility function to find the opposite of a corner.
+	 * \brief Utility function to find the diagonal opposite of a corner.
 	 *
 	 * The diagonally opposed corner is the "opposite" of a given corner.
 	 * Example : TopLeft is diagonally opposed to BottomRight and vice-versa.
@@ -21,5 +21,23 @@ namespace ch {
 	 * \returns The opposite of the given corner.
 	 */
 	constexpr Corner diagonally_opposed_corner(Corner corner);
+
+	/**
+	 * \brief Utility function to find the vertical opposite of a corner.
+	 *
+	 * Example : TopLeft is vertically opposed to BottomLeft and vice-versa.
+	 *
+	 * \returns The vertical opposite of the given corner.
+	 */
+	constexpr Corner vertically_opposed_corner(Corner corner);
+
+	/**
+	 * \brief Utility function to find the horizontal opposite of a corner.
+	 *
+	 * Example : TopLeft is horizontally opposed to TopRight and vice-versa.
+	 *
+	 * \returns The horizontal opposite of the given corner.
+	 */
+	constexpr Corner horizontally_opposed_corner(Corner corner);
 }
 
