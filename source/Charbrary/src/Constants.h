8d6dbb326e2dcad11cceae915801cafff6006f36
Lucas Charbonnier <lcs.charbonnier@gmail.com>
2021-04-22 11:42:21 +0200

    fixed direction vectors so that are compatible with a desktop coordinate system (top left origin)

--- source/Charbrary/src/Constants.h
+++ source/Charbrary/src/Constants.h
@@ -5,10 +5,12 @@
 namespace ch {
 	constexpr float FLT_PI = 3.14159265359f;
 
+	// The charbrary use a top-left-origin coordinate system. Below are constants for the direction of each axis.
+
 	static const ch::vec_t LEFT_VEC = { -1.f, 0.f };
 	static const ch::vec_t RIGHT_VEC = { 1.f, 0.f };
-	static const ch::vec_t UP_VEC = { 0.f, 1.f };
-	static const ch::vec_t DOWN_VEC = { 0.f, -1.f };
+	static const ch::vec_t UP_VEC = { 0.f, -1.f };
+	static const ch::vec_t DOWN_VEC = { 0.f, 1.f };
 
 	static const ch::vec_t NULL_VEC = { 0.f,0.f };
 }
